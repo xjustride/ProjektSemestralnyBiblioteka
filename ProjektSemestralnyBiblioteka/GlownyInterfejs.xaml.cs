7870dae32b9bfa958ba9bce827e16219302286ff
xjustride <remigiusz.krupa@microsoft.wsei.edu.pl>
2023-06-10 14:26:29 +0200

    dodanie możliwości wprowadzania książek i edycja interfejsu

--- ProjektSemestralnyBiblioteka/GlownyInterfejs.xaml.cs
+++ ProjektSemestralnyBiblioteka/GlownyInterfejs.xaml.cs
@@ -19,14 +19,72 @@ namespace ProjektSemestralnyBiblioteka
 	/// </summary>
 	public partial class GlownyInterfejs : Window
 	{
+		private ProjektSemContext dbContext = new ProjektSemContext();
+
 		public GlownyInterfejs()
 		{
 			InitializeComponent();
+			LoadKategorie();
 		}
 
+		private void LoadKategorie()
+		{
+			var kategorie = dbContext.Kategories.ToList();
+			cmbKategoria.ItemsSource = kategorie;
+		}
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			using (var context = new ProjektSemContext())
+			{
+				List<Kategorie> kategorie = context.Kategories.ToList();
+				cmbKategoria.ItemsSource = kategorie;
+			}
+		}
 		private void DodajKsiazke_Click(object sender, RoutedEventArgs e)
 		{
+			string tytul = txtTytul.Text;
+			string autor = txtAutor.Text;
+			int rokWydania;
 
+			if (!int.TryParse(txtRokWydania.Text, out rokWydania))
+			{
+				MessageBox.Show("Podano nieprawidłowy rok wydania.");
+				return;
+			}
+
+			Kategorie wybranaKategoria = cmbKategoria.SelectedItem as Kategorie;
+
+			if (string.IsNullOrEmpty(tytul) || string.IsNullOrEmpty(autor) || wybranaKategoria == null)
+			{
+				MessageBox.Show("Wprowadź wszystkie dane.");
+				return;
+			}
+
+			try
+			{
+				Ksiazki nowaKsiazka = new Ksiazki()
+				{
+					Id = dbContext.Ksiazkis.Max(c => c.Id) + 1,
+					Tytul = tytul,
+					Autor = autor,
+					RokWydania = rokWydania,
+				};
+
+				dbContext.Ksiazkis.Add(nowaKsiazka);
+				dbContext.SaveChanges();
+
+				MessageBox.Show("Dodano nową książkę.");
+
+				// Wyczyść pola tekstowe po dodaniu książki
+				txtTytul.Text = string.Empty;
+				txtAutor.Text = string.Empty;
+				txtRokWydania.Text = string.Empty;
+				cmbKategoria.SelectedIndex = -1;
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Wystąpił błąd podczas dodawania książki: " + ex.Message);
+			}
 		}
 
 		private void Powrot_Click(object sender, RoutedEventArgs e)
