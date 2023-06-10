007cc233b6fdb5073c99ef8fdd1bf497e55e0e5f
xjustride <remigiusz.krupa@microsoft.wsei.edu.pl>
2023-06-10 14:16:12 +0200

    dodanie możliwości wprowadzania czytelników w oknie DodawanieCzytelnikow.xaml

--- ProjektSemestralnyBiblioteka/Czytelnicy.cs
+++ ProjektSemestralnyBiblioteka/Czytelnicy.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjektSemestralnyBiblioteka;
 
 public partial class Czytelnicy
 {
-    public int Id { get; set; }
+	[Key]
+	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+	public int Id { get; set; }
 
-    public string Imie { get; set; } = null!;
+	public string Imie { get; set; } = null!;
 
     public string Nazwisko { get; set; } = null!;
 
     public string NumerTelefonu { get; set; } = null!;
 
-    public virtual ICollection<Wypozyczenium> Wypozyczenia { get; set; } = new List<Wypozyczenium>();
+	public virtual ICollection<Wypozyczenium> Wypozyczenia { get; set; } = new List<Wypozyczenium>();
 }
--- ProjektSemestralnyBiblioteka/DodawanieCzytelnikow.xaml.cs
+++ ProjektSemestralnyBiblioteka/DodawanieCzytelnikow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,6 +20,8 @@ namespace ProjektSemestralnyBiblioteka
 	/// </summary>
 	public partial class DodawanieCzytelnikow : Window
 	{
+		private ProjektSemContext dbContext = new ProjektSemContext();
+
 		public DodawanieCzytelnikow()
 		{
 			InitializeComponent();
@@ -26,8 +29,42 @@ namespace ProjektSemestralnyBiblioteka
 
 		private void DodajCzytelnika_Click(object sender, RoutedEventArgs e)
 		{
+			string imie = txtImie.Text;
+			string nazwisko = txtNazwisko.Text;
+			string numerTelefonu = txtNumerTelefonu.Text;
 
-        }
+			if (string.IsNullOrEmpty(imie) || string.IsNullOrEmpty(nazwisko) || string.IsNullOrEmpty(numerTelefonu))
+			{
+				MessageBox.Show("Wprowadź wszystkie dane.");
+				return;
+			}
+
+			try
+			{
+				Czytelnicy nowyCzytelnik = new Czytelnicy()
+				{
+					Id = dbContext.Czytelnicies.Max(c => c.Id) + 1,
+					Imie = imie,
+					Nazwisko = nazwisko,
+					NumerTelefonu = numerTelefonu
+				};
+
+				dbContext.Czytelnicies.Add(nowyCzytelnik);
+				dbContext.SaveChanges();
+
+				MessageBox.Show("Dodano nowego czytelnika.");
+
+				// Wyczyść pola tekstowe po dodaniu czytelnika
+				txtImie.Text = string.Empty;
+				txtNazwisko.Text = string.Empty;
+				txtNumerTelefonu.Text = string.Empty;
+			}
+			catch (Exception ex)
+			{
+				//MessageBox.Show("Wystąpił błąd podczas dodawania czytelnika: " + ex.Message);
+				MessageBox.Show("Wystąpił błąd podczas zapisywania zmian: " + ex.InnerException?.Message);
+			}
+		}
 
 		private void Powrot_Click(object sender, RoutedEventArgs e)
 		{
