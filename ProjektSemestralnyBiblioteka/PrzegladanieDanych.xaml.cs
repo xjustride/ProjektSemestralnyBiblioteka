d77d460238f040a2caec2e40630db363a3e57147
xjustride <remigiusz.krupa@microsoft.wsei.edu.pl>
2023-06-10 15:15:19 +0200

    zmiana interfejsu zeby odpowiednio wyswietlala sie lista w interfejsie przegladanie danych

--- ProjektSemestralnyBiblioteka/PrzegladanieDanych.xaml.cs
+++ ProjektSemestralnyBiblioteka/PrzegladanieDanych.xaml.cs
@@ -1,40 +1,74 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace ProjektSemestralnyBiblioteka
 {
-	/// <summary>
-	/// Logika interakcji dla klasy PrzegladanieDanych.xaml
-	/// </summary>
 	public partial class PrzegladanieDanych : Window
 	{
+		private List<Wypozyczenium> wypozyczenia;
+
 		public PrzegladanieDanych()
 		{
 			InitializeComponent();
+
+			// Inicjalizacja danych wypożyczeń
+			wypozyczenia = PobierzWypozyczeniaZBazyDanych();
+			lstDane.ItemsSource = wypozyczenia;
+		}
+
+		private List<Wypozyczenium> PobierzWypozyczeniaZBazyDanych()
+		{
+			using (var context = new ProjektSemContext())
+			{
+				return context.Wypozyczenia.Include("Ksiazka").Include("Czytelnik").ToList();
+			}
 		}
 
 		private void Usun_Click(object sender, RoutedEventArgs e)
 		{
+			// Sprawdź, czy został wybrany element do usunięcia
+			if (lstDane.SelectedItem == null)
+			{
+				MessageBox.Show("Proszę wybrać element do usunięcia.");
+				return;
+			}
 
-        }
+			// Pobierz wybrane wypożyczenie
+			var wybraneWypozyczenie = lstDane.SelectedItem as Wypozyczenium;
+
+			using (var context = new ProjektSemContext())
+			{
+				// Sprawdź, czy wypożyczenie istnieje w bazie danych
+				var wypozyczenie = context.Wypozyczenia.Find(wybraneWypozyczenie.Id);
+
+				if (wypozyczenie == null)
+				{
+					MessageBox.Show("Wybrane wypożyczenie nie istnieje.");
+					return;
+				}
+
+				// Usuń wypożyczenie z bazy danych
+				context.Wypozyczenia.Remove(wypozyczenie);
+				context.SaveChanges();
+			}
+
+			// Odśwież widok listy danych
+			wypozyczenia.Remove(wybraneWypozyczenie);
+			lstDane.ItemsSource = null;
+			lstDane.ItemsSource = wypozyczenia;
+
+			MessageBox.Show("Wypożyczenie zostało usunięte.");
+		}
 
 		private void Powrot_Click(object sender, RoutedEventArgs e)
 		{
-			MainWindow mw = new MainWindow();
-			mw.Show();
-			this.Close();
-
+			// Zamknij okno przeglądania danych i wróć do poprzedniego okna (MainWindow)
+			MainWindow mainWindow = new MainWindow();
+			mainWindow.Show();
+			Close();
 		}
 	}
 }
